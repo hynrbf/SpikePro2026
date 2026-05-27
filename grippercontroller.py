146a9dfeb12c3cd057c4eaee5629d93303bfcd1a
Grachi <hynrbf@gmail.com>
2026-05-27 15:51:07 +0800

    z2

--- grippercontroller.py
+++ grippercontroller.py
@@ -13,11 +13,11 @@ class GripperController:
         await GripperController.__right_motor.run_target(Speed.Fast, 0, Stop.COAST)
 
     @staticmethod
-    async def lift_left(degree: float = 80):
+    async def lift_left(degree: float = 80, speed: float = Speed.Fast):
         degree = degree if degree >= 0 else 0
-        await GripperController.__left_motor.run_target(Speed.Fast, degree, Stop.HOLD)
+        await GripperController.__left_motor.run_target(speed, degree, Stop.HOLD)
 
     @staticmethod
-    async def lift_right(degree: float = 80):
+    async def lift_right(degree: float = 80, speed: float = Speed.Fast):
         degree = degree if degree <= 0 else degree * -1
-        await GripperController.__right_motor.run_target(Speed.Fast, degree, Stop.HOLD)
+        await GripperController.__right_motor.run_target(speed, degree, Stop.HOLD)
