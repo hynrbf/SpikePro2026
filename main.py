e098ee84c35d19f72b77fa592bd1ee1ac60ca044
Grachi <hynrbf@gmail.com>
2026-05-28 13:36:15 +0800

    g1

--- main.py
+++ main.py
@@ -1,63 +1,20 @@
 from pybricks.tools import run_task
 from pybricks import version
-from pybricks.tools import multitask, wait
+# from pybricks.tools import multitask, wait
 
-from handcontroller import HandController
-from shared import Speed
-from wheelcontroller import WheelController
+# from handcontroller import HandController
+from mainrandomized import MissionRandomized
+
+
+# from shared import Speed
+# from wheelcontroller import WheelController
 
 
 async def main():
     print("\nStart, pb version: ", version)
 
     try:
-        await WheelController.move_forward(450)
-        await WheelController.left_turn()
-        await WheelController.move_backward(520, with_brake=True)
-        await WheelController.move_forward(180)
-        await WheelController.right_turn()
-        await WheelController.move_forward(230)
-        await WheelController.move_forward(125)
-        await WheelController.move_backward(120)
-        await multitask(HandController.lift_left(speed=Speed.Slow),
-                        HandController.lift_right(speed=Speed.Slow))
-        await WheelController.right_turn()
-        await WheelController.move_forward(125, speed=Speed.Slow)
-        await multitask(HandController.lift_left(30, speed=Speed.Slow),
-                        HandController.lift_right(30, speed=Speed.Slow))
-        await multitask(WheelController.move_backward(100, speed=Speed.Slow),
-                        HandController.lift_left(17, speed=Speed.Slow),
-                        HandController.lift_right(17, speed=Speed.Slow))
-        await WheelController.move_backward(850, with_brake=True)
-        await WheelController.move_forward(300)
-        await WheelController.right_turn(180)
-        await multitask(HandController.lift_left(speed=Speed.Slow),
-                        HandController.lift_right(speed=Speed.Slow))
-        await WheelController.move_forward(200, speed=Speed.Slow)
-        await wait(500)
-        await WheelController.move_backward(900, with_brake=True)
-        await WheelController.move_forward(180)
-        await WheelController.right_turn()
-        await WheelController.move_forward(265)
-        await WheelController.move_forward(135)
-        await WheelController.move_backward(100)
-        await multitask(HandController.lift_left(speed=Speed.Slow),
-                        HandController.lift_right(speed=Speed.Slow))
-        await WheelController.right_turn()
-        await WheelController.move_forward(130, speed=Speed.Slow)
-        await multitask(HandController.lift_left(30, speed=Speed.Slow),
-                        HandController.lift_right(30, speed=Speed.Slow))
-        await multitask(WheelController.move_backward(100, speed=Speed.Slow),
-                        HandController.lift_left(17, speed=Speed.Slow),
-                        HandController.lift_right(17, speed=Speed.Slow))
-        await WheelController.move_backward(850, with_brake=True)
-        await WheelController.move_forward(350)
-        await WheelController.left_turn()
-        await WheelController.move_forward(95)
-        await WheelController.left_turn()
-        await multitask(HandController.lift_left(speed=Speed.Slow),
-                        HandController.lift_right(speed=Speed.Slow))
-        await WheelController.move_forward(275, speed=Speed.Slow)
+        await MissionRandomized.exec_mission()
 
         # done only, do not remove
     finally:
---
+++ mainrandomized.py
@@ -0,0 +1,58 @@
+from handcontroller import HandController
+from shared import Speed
+from wheelcontroller import WheelController
+from pybricks.tools import multitask, wait
+
+
+class MissionRandomized:
+    @staticmethod
+    async def exec_mission():
+        await WheelController.move_forward(450)
+        await WheelController.left_turn()
+        await WheelController.move_backward(520, with_brake=True)
+        await WheelController.move_forward(180)
+        await WheelController.right_turn()
+        await WheelController.move_forward(230)
+        await WheelController.move_forward(125)
+        await WheelController.move_backward(120)
+        await multitask(HandController.lift_left(speed=Speed.Slow),
+                        HandController.lift_right(speed=Speed.Slow))
+        await WheelController.right_turn()
+        await WheelController.move_forward(125, speed=Speed.Slow)
+        await multitask(HandController.lift_left(30, speed=Speed.Slow),
+                        HandController.lift_right(30, speed=Speed.Slow))
+        await multitask(WheelController.move_backward(100, speed=Speed.Slow),
+                        HandController.lift_left(17, speed=Speed.Slow),
+                        HandController.lift_right(17, speed=Speed.Slow))
+        await WheelController.move_backward(850, with_brake=True)
+        await WheelController.move_forward(300)
+        await WheelController.right_turn()
+        await WheelController.move_forward(20)
+        await WheelController.right_turn()
+        await multitask(HandController.lift_left(speed=Speed.Slow),
+                        HandController.lift_right(speed=Speed.Slow))
+        await WheelController.move_forward(200, speed=Speed.Slow)
+        await wait(500)
+        await WheelController.move_backward(900, with_brake=True)
+        await WheelController.move_forward(180)
+        await WheelController.right_turn()
+        await WheelController.move_forward(255)
+        await WheelController.move_forward(135)
+        await WheelController.move_backward(100)
+        await multitask(HandController.lift_left(speed=Speed.Slow),
+                        HandController.lift_right(speed=Speed.Slow))
+        await WheelController.right_turn()
+        await WheelController.move_forward(130, speed=Speed.Slow)
+        await multitask(HandController.lift_left(30, speed=Speed.Slow),
+                        HandController.lift_right(30, speed=Speed.Slow))
+        await multitask(WheelController.move_backward(100, speed=Speed.Slow),
+                        HandController.lift_left(17, speed=Speed.Slow),
+                        HandController.lift_right(17, speed=Speed.Slow))
+        await WheelController.move_backward(850, with_brake=True)
+        await WheelController.move_forward(350)
+        await WheelController.left_turn()
+        await WheelController.move_forward(95)
+        await WheelController.left_turn()
+        await multitask(HandController.lift_left(speed=Speed.Slow),
+                        HandController.lift_right(speed=Speed.Slow))
+        await WheelController.move_forward(275, speed=Speed.Slow)
