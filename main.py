192ccf8d7677790fc6ec1614a97552d7d092d4c5
Grachi <hynrbf@gmail.com>
2026-05-28 11:09:36 +0800

    z2

--- main.py
+++ main.py
@@ -35,31 +35,6 @@ async def main():
                         HandController.lift_right(speed=Speed.Slow))
         await WheelController.move_forward(200, speed=Speed.Slow)
         await wait(500)
-        # await WheelController.move_backward(355)
-        # await WheelController.left_turn()
-        # await WheelController.move_forward(120)
-        # await WheelController.right_turn()
-        # await WheelController.move_forward(258)
-        # await WheelController.right_turn()
-        # await WheelController.move_forward(260, Speed.Slow, with_brake=True)
-        # await multitask(WheelController.move_backward(30), HandController.lift_left(speed=Speed.Slow),
-        #                HandController.lift_right(speed=Speed.Slow))
-        # await WheelController.move_backward(200)
-        # await WheelController.left_turn()
-        # await WheelController.move_backward(120)
-        # await WheelController.move_backward(float(500))
-        # await WheelController.move_forward(float(390))
-        # await WheelController.left_turn(float(83))
-        # await WheelController.follow_the_line(120)
-        # await WheelController.follow_the_line(150, 0.90)
-        # await GripperController.reset()
-        # await GripperController.up()
-        # await wait(100)
-        # await GripperController.down()
-
-        # await GripperController.close()
-        # await wait(100)
-        # await GripperController.opening()
 
         # done only, do not remove
     finally:
