25c3764b442c61b8862e7e794d3847431b5eaa18
Grachi <hynrbf@gmail.com>
2026-06-06 13:33:45 +0800

    ci

--- mainyellowtower.py
+++ mainyellowtower.py
@@ -1,4 +1,4 @@
-from pybricks.tools import wait  # multitask
+from pybricks.tools import wait, multitask  # multitask
 
 from colorcontroller import MatColor
 from handcontroller import HandController
@@ -27,15 +27,22 @@ class MissionYellowTower:
         await WheelController.move_forward(50, speed=Speed.Medium)
         await HandController.lift_left(55)
         await WheelController.move_backward(300, with_brake=True, speed=Speed.Medium)
-        await HandController.lift_left(30)
+        await HandController.lift_left(35)
         await WheelController.right_turn()
         await WheelController.move_forward(1050, speed=Speed.Medium)
         await WheelController.left_turn()
         await WheelController.move_backward(670, with_brake=True)
+        # Straightening of yellow towers
+        await multitask(HandController.lift_right(0, speed=Speed.Slow),
+                        HandController.lift_left(0, speed=Speed.Slow))
+        await wait(200)
+        await multitask(HandController.lift_right(35, speed=Speed.Slow),
+                        HandController.lift_left(35, speed=Speed.Slow))
         await WheelController.move_forward(220, speed=Speed.Medium)
         await WheelController.left_turn()
         await WheelController.move_towards_mat_color(MatColor.Black, Speed.Slow)
         await WheelController.right_turn(180)
+        await wait(500)
         # placing 1st tower
         await WheelController.move_forward(275, speed=Speed.Medium)
         await HandController.lift_right(80)
