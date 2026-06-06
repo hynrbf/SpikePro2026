203fe30b32154bd2d6975fecfd4f6813889c9c14
Grachi <hynrbf@gmail.com>
2026-06-06 22:18:05 +0800

    s1

---
+++ _strategy1/main.py
@@ -0,0 +1,18 @@
+from pybricks.tools import run_task  # , wait
+from pybricks import version
+
+from mainrandomized import MissionRandomized
+from mainredtower import MissionRedTower
+from mainyellowtower import MissionYellowTower
+
+
+async def main():
+    print("\nStart, pb version: ", version)
+
+    await MissionRandomized.exec_mission()
+    await MissionYellowTower.exec_mission()
+    await MissionRedTower.exec_mission()
+    print("DONE!")
+
+
+run_task(main())
