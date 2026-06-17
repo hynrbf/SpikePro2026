c441dfd85db36c1ff6562baa8d908f93ab936009
chito <chito@richdale.co.uk>
2026-06-17 09:27:10 +0800

    save

--- colorcontroller.py
+++ colorcontroller.py
@@ -3,10 +3,10 @@ from pybricks.parameters import Port
 
 
 class MatColor:
-    Green = 180
-    DarkGreen = 140
-    LightGreen = 153
     Black = 240
+    DarkGreen = 140
+    Green = 180
+    LightGreen = 153
     BlackTwo = 220  # 210 #216
     Maroon = 349
     Red = 349
