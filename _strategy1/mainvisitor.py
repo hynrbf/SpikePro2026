a01dc93d3bce1700c9d360171d079e76f3d39217
chito <chito@richdale.co.uk>
2026-06-19 20:42:06 +0800

    start 2

--- _strategy1/mainvisitor.py
+++ _strategy1/mainvisitor.py
@@ -1,4 +1,4 @@
-from pybricks.tools import multitask
+from pybricks.tools import multitask, wait
 
 from handcontroller import HandController
 from shared import Speed
@@ -11,11 +11,13 @@ class MissionVisitor:
         await multitask(WheelController.move_forward(40),
                         HandController.lift_right(), HandController.lift_left())
         await WheelController.left_turn()
+
         # 1st bangga
         await  WheelController.move_backward(450, with_brake=True)
         await multitask(WheelController.move_forward(73), HandController.lift_right(),
                         HandController.lift_left())
         await WheelController.right_turn()
+
         # getting 2 visitors slowly
         await multitask(HandController.lift_left(10), HandController.lift_right(10))
         await WheelController.move_forward(40, speed=Speed.Slow)
@@ -27,12 +29,13 @@ class MissionVisitor:
         await WheelController.left_turn(30, turn_speed=30)
         await WheelController.move_forward(250, speed=Speed.Slow)
         await multitask(HandController.lift_right(-10, speed=Speed.Slow),
-                        HandController.lift_left(-8, speed=Speed.Slow))
+                        HandController.lift_left(-3, speed=Speed.Slow))
         await WheelController.move_backward(90, speed=Speed.Slow)
         await multitask(HandController.lift_right(10),
                         HandController.lift_left())
         await WheelController.move_forward(145, speed=Speed.Slow)
         await HandController.lift_left(15)
+
         # putting green visitor
         await WheelController.move_forward(100, speed=Speed.Slow)
         await WheelController.right_turn(40, turn_speed=30)
@@ -42,12 +45,15 @@ class MissionVisitor:
         await WheelController.move_forward(125, speed=Speed.Medium)
         await HandController.lift_right(0)
         await WheelController.move_backward(145, speed=Speed.Slow)
+
         # going to the other side
         await WheelController.left_turn(140, turn_speed=90)
         await WheelController.move_forward(500)
+
         # 2nd bangga
         await WheelController.move_forward(190, speed=Speed.Slow, with_brake=True)
         await WheelController.move_backward(50, speed=Speed.Medium)
+
         # putting the red visitor
         await WheelController.right_turn(turn_speed=90)
         await WheelController.move_forward(100)
@@ -56,3 +62,4 @@ class MissionVisitor:
         await WheelController.move_backward(15, speed=Speed.Medium)
         await HandController.lift_left(speed=Speed.Slow)
         await WheelController.move_forward(15, speed=Speed.Slow)
+        await wait(100)
