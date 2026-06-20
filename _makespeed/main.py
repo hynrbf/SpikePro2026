6cb78a4c938f77a2a8975c31da260a19bb4ebdc2
hynrbf <126869603+hynrbf@users.noreply.github.com>
2026-06-20 20:36:39 +0800

    c1

---
+++ _makespeed/main.py
@@ -0,0 +1,18 @@
+from pybricks.tools import run_task  # , wait
+from pybricks import version
+
+from mainredtower import MissionRedTower
+from mainyellowtower import MissionYellowTower
+from mainvisitor import MissionVisitor
+
+
+async def main():
+    print("\nStart, pb version: ", version)
+
+    await MissionYellowTower.exec_mission()
+    await MissionRedTower.exec_mission()
+    await MissionVisitor.exec_mission()
+    print("DONE!")
+
+
+run_task(main())
