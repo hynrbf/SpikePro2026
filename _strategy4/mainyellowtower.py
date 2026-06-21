31ffa5aeb35671bc3b85c609016d3ba88b95ab98
hynrbf <hynrbf@gmail.com>
2026-06-21 15:52:45 +0800

    c1

--- _strategy4/mainyellowtower.py
+++ _strategy4/mainyellowtower.py
@@ -15,10 +15,10 @@ class MissionYellowTower:
         await WheelController.move_backward(500, with_brake=True)
         await WheelController.move_forward(250)
         await WheelController.left_turn()
-        await WheelController.move_forward(125)
+        await WheelController.move_forward(210)
         await WheelController.move_towards_mat_color(MatColor.Black, speed=Speed.Slow)
         await WheelController.right_turn(180)
-        await WheelController.move_backward(25)
+        await WheelController.move_backward(15)
         await multitask(HandController.lift_left(0), HandController.lift_right(0),
                         WheelController.right_turn())
 
@@ -36,7 +36,7 @@ class MissionYellowTower:
 
         # LOOOOOOOONG DRIVE
         await WheelController.move_forward(825)
-        await WheelController.move_towards_mat_color(MatColor.Black)
+        await WheelController.move_towards_mat_color(MatColor.Black, speed=Speed.Slow)
 
         # Straightening of yellow towers
         await WheelController.left_turn(turn_speed=90)
@@ -48,7 +48,6 @@ class MissionYellowTower:
         await WheelController.move_backward(150, speed=Speed.Medium, with_brake=True)
         await multitask(HandController.lift_left(-10), HandController.lift_right(-10))
         await WheelController.move_forward(400, speed=Speed.Medium)  # float is: 85 in game map; 91 in BGBES
-        # await WheelController.move_towards_mat_color(MatColor.DarkGreen)
         await multitask(HandController.lift_left(32), HandController.lift_right(32))
 
         # placing 1st yellow tower
@@ -70,7 +69,7 @@ class MissionYellowTower:
         await WheelController.left_turn(turn_speed=90)
         await multitask(HandController.lift_left(-10, speed=Speed.Slow),
                         HandController.lift_right(80, speed=Speed.Slow))
-        await WheelController.move_forward(225, speed=Speed.Medium)
+        await WheelController.move_forward(230, speed=Speed.Medium)
         await multitask(HandController.lift_right(40, speed=Speed.Slow), HandController.lift_left(28))
         await WheelController.move_backward(150, speed=Speed.Slow)
         await multitask(HandController.lift_left(-10, speed=Speed.Slow), HandController.lift_right(-10))
